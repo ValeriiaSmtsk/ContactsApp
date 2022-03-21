8e35377d24b9ce72d566657aadaa053f5dca05ae
Salnik Valeriia <melfed.salnik@gmail.com>
2022-03-22 02:21:46 +0300

    initial commit

---
+++ ContactsApp/DataManager.swift
@@ -0,0 +1,58 @@
+//
+//  DataManager.swift
+//  ContactsApp
+//
+//  Created by Валерия Смецкая on 22.03.2022.
+//
+
+let name = [
+    "John",
+    "Sharon",
+    "Aaron",
+    "Steven",
+    "Nicola",
+    "Ted",
+    "Bruce",
+    "Carl",
+    "Allan",
+    "Tim"
+]
+
+let surname = [
+    "Murphy",
+    "Jankin",
+    "Williams",
+    "Black",
+    "Robertson",
+    "Bulter",
+    "Isaacson",
+    "Smith",
+    "Pennyworth",
+    "Cook"
+]
+
+let phoneNumber = [
+    145684833,
+    125335554,
+    385675556,
+    345999603,
+    389856740,
+    104332270,
+    123456789,
+    445566770,
+    350001711,
+    756669119
+]
+
+let email = [
+    "swift@gmail.com",
+    "apple@gmail.com",
+    "my@gmail.com",
+    "mail@yahoo.com",
+    "bank@yahoo.com",
+    "job@yahoo.com",
+    "123@list.ru",
+    "abc@gmail.com",
+    "456@yahoo.com",
+    "abrakadabra@gmail.com"
+]
